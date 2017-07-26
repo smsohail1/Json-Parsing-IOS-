71cdbeaabd03126b4aec0436a0e7e5a655e09649
Abdul Ahad <abdulahad@Abduls-iMac.local>
2017-07-25 22:01:24 -0700

    Initial Commit

---
+++ Rest Api JSON ParsingTests/Rest_Api_JSON_ParsingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rest_Api_JSON_ParsingTests.swift
+//  Rest Api JSON ParsingTests
+//
+//  Created by Abdul Ahad on 7/25/17.
+//  Copyright © 2017 plash. All rights reserved.
+//
+
+import XCTest
+@testable import Rest_Api_JSON_Parsing
+
+class Rest_Api_JSON_ParsingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
